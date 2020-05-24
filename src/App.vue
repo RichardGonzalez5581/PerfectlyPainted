aef0b62b5a66cbe39e4509e39f60c7a07faba1ad
RichardGonzalez5581 <RichardGonzalez5581@gmail.com>
2020-05-24 06:09:55 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,53 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+  <v-app light>
+    <v-navigation-drawer v-model="sideNav" absolute temporary>
+    <v-card class="mx-auto" max-width="300" tile>
+    <v-list flat>
+      <v-subheader>Perfectly Painted</v-subheader>
+      <v-list-item-group v-model="item" color="primary">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+  </v-list>
+  </v-card>
+    </v-navigation-drawer>
+    <v-card>
+    <v-toolbar dark style="background-color: rgb(102, 45, 145)">
+      <v-app-bar-nav-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-app-bar-nav-icon>
+      <v-toolbar-title>Perfectly Painted</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn text v-for="(item, i) in items" :key= "i" v-text="item.text"></v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    </v-card>
+    <main>
+      <router-view></router-view>
+    </main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    sideNav: false,
+    items: [
+      { text: 'Home'},
+      { text: 'Services'},
+      { text: 'Gallery'},
+      { text: 'Reviews'},
+      { text: 'About Us'},
+      { text: 'Contact'}
+    ]
   }),
 };
 </script>
